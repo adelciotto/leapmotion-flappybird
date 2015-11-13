03e1cf3ec328ed4756e2bf8addcda07f2d77f8b1
Anthony Del Ciotto <anthdel.developer@gmail.com>
2015-11-13 13:24:14 +1100

    removed redundant mobile scale config

--- src/states/boot.js
+++ src/states/boot.js
@@ -19,17 +19,10 @@ export default class BootState extends Phaser.State {
         this.physics.startSystem(Phaser.Physics.ARCADE);
 
         // configure the scale manager
-        const ratio = this._getRatio('all', constants.GAME_WIDTH, constants.GAME_HEIGHT);
-        if (navigator.isCocoonJS) {
-            this.game.world._container.scale.x = ratio.x;
-            this.game.world._container.scale.y = ratio.y;
-            this.game.world._container.updateTransform();
-        } else {
-            this.scale.minWidth = constants.GAME_WIDTH;
-            this.scale.minHeight = constants.GAME_HEIGHT;
-            this.scale.pageAlignHorizontally = this.scale.pageAlignVertically = true;
-            this.scale.scaleMode = this.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
-        }
+        this.scale.minWidth = constants.GAME_WIDTH;
+        this.scale.minHeight = constants.GAME_HEIGHT;
+        this.scale.pageAlignHorizontally = this.scale.pageAlignVertically = true;
+        this.scale.scaleMode = this.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
 
         // capture certain keys to prevent their default actions in the browser.
         // this is only necessary because this is an HTML5 game.
@@ -40,28 +33,5 @@ export default class BootState extends Phaser.State {
         this.input.maxPointers = 1;
 
         this.state.start('preload');
-    }
-
-    _getRatio(type, w, h) {
-        const scaleX = this.game.width / w;
-        const scaleY = this.game.height / h;
-        const result = { x: 1, y: 1 };
-
-        switch(type) {
-            case 'all':
-                result.x = scaleX > scaleY ? scaleY : scaleX;
-                result.y = scaleX > scaleY ? scaleY : scaleX;
-                break;
-            case 'fit':
-                result.x = scaleX > scaleY ? scaleX : scaleY;
-                result.y = scaleX > scaleY ? scaleX : scaleY;
-                break;
-            case 'fill':
-                result.x = scaleX;
-                result.y = scaleY;
-                break;
-        }
-
-        return result;
     }
 }
